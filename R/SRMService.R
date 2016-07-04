185dfd7b23cc5c6dc7f895cd5e4b201374466ca7
Witold Wolski <wewolski@gmail.com>
2016-07-04 18:22:53 +0200

    added methods for filtering for NA's and plotting heatmaps with data to show data completeness.

--- R/SRMService.R
+++ R/SRMService.R
@@ -20,21 +20,30 @@
 #' tmp <- "D:/googledrive/DataAnalysis/p1930"
 #' allData <- read.csv( file=file.path(tmp,"data/longFormat.txt"),row.names = 1)
 #' data <-allData
-#' pool=1
+#' pool=2
 #' data <-allData[allData$pool==pool,]
-#' colnames(data)
-#' length(unique(data$Replicate.Name))
 #' srms <- SRMService(data,qvalue=0.25)
 #' srms$plotQValues()
 #' srms$plotTransition()
 #' srms$plotTransition(light=TRUE)
 #' srms$getNrNAs()
 #' srms$getNrNAs(light=TRUE)
-#' tmpH <- srms$getTransitionIntensities()
-#' head(tmpH)
-#' tmpL <- srms$getTransitionIntensities(light=TRUE)
+#' srms$maxNAHeavy
+#' srms$maxNALight
+#' hist(srms$piw$nrNA)
 #'
-#' head(tmpL)
+#' srms$setMaxNAHeavy(30)
+#' srms$setMaxNALight(40)
+#'
+#' tmpH <- srms$getTransitionIntensities()
+#' dim(tmpH)
+#' stopifnot(max(apply(tmpH, 1, function(x){sum(is.na(x))}))<=30)
+#' tmpL <- srms$getTransitionIntensities(light=TRUE)
+#' dim(tmpL)
+#' max(apply(tmpL, 1, function(x){sum(is.na(x))}))
+#' x<-srms$getMatchingIntensities()
+#' dim(x$light)
+#' tmp<-srms$getLHLog2FoldChange()
 #'
 SRMService <- setRefClass("SRMService",
                           fields = list( data = "data.frame",
@@ -83,20 +92,65 @@ SRMService <- setRefClass("SRMService",
                               hist(.self$data$annotation_QValue)
                               abline(v=.self$qValueThreshold,col=2)
                             },
-
                             getNrNAs = function(light = FALSE){
                               "show nr of NA's for heavy (defalt) or light transitions"
-
                               isolable<-ifelse(light, "light", "heavy")
                               nas <-subset(.self$piw,Isotope.Label==isolable)$nrNA
                               hist(nas, main=isolable)
                               abline(v=ifelse(light, .self$maxNALight, .self$maxNAHeavy), col=2)
                               invisible(nas)
                             },
-
+                            setMaxNAHeavy = function(max=0){
+                              "set maximum of na's heavy row"
+                              .self$maxNAHeavy = max
+                            },
+                            setMaxNALight = function(max=0){
+                              "set maximum of na's in light row"
+                              .self$maxNALight = max
+                            },
                             getTransitionIntensities=function(light=FALSE){
-                              int_ <- subset(.self$int, .self$piw$Isotope.Label==ifelse(light, "light", "heavy"))
+                              "get matrix with intensities, where nr of NAs in row < maxNA"
+                              idx <- .self$piw$nrNA <= ifelse(light,.self$maxNALight, .self$maxNAHeavy)
+
+                              int_ <- subset(.self$int, .self$piw$Isotope.Label==ifelse(light, "light", "heavy")
+                                             & idx
+                              )
                               return(int_)
+                            },
+                            getMatchingIntensities = function(){
+                              "get matrix with intensities, where nr of NAs in row < maxNA"
+                              intLight_ <- .self$getTransitionIntensities(light=TRUE)
+                              intHeavy_ <- .self$getTransitionIntensities()
+
+                              rownames(intLight_) <- gsub("_light$","",rownames(intLight_))
+                              rownames(intHeavy_) <- gsub("_heavy$","",rownames(intHeavy_))
+
+                              idx <-intersect(rownames(intLight_),rownames(intHeavy_))
+
+                              return(list(light = intLight_[idx,], heavy = intHeavy_[idx,]))
+                            },
+                            plotCommonTransitions = function(light=FALSE){
+                              "Shows transitions which occure in heavy and light"
+                              int_ <- getMatchingIntensities()
+                              int_ <- ifelse(light,int_$light, int_$heavy)
+                              imageWithLabels(log2(t((int_))),col = quantable::getRedScale(),
+                                              main="heavy Int",marLeft=c(5,15,3,3),marRight = c(5,0,3,3))
+                              invisible(int_)
+                            },
+                            getLHLog2FoldChange = function(plot=TRUE){
+                              int_<-getMatchingIntensities()
+                              stopifnot(colnames( int_$light) == colnames(int_$heavy))
+                              stopifnot(rownames( int_$light) == rownames(int_$heavy))
+
+                              logfc <-(log2(( int_$light )) - log2((int_$heavy)) )
+
+                              if(plot){
+                                imageWithLabels(t(logfc), main="log2(L/H)", col= getBlueWhiteRed(), marLeft=c(5,15,3,3),marRight = c(5,0,3,3))
+                                invisible(logfc)
+                              }
+                              else{
+                                return(logfc)
+                              }
                             },
 
                             plotTransition = function(light = FALSE){
