6291664ecb6b6f27205b364b59ce8b830fee118e
Witold Wolski <wewolski@gmail.com>
2016-09-01 15:38:08 +0200

    test

--- startserver.bat
+++ startserver.bat
@@ -1,1 +1,2 @@
-R -e "shiny::runApp('C:/Users/wolski/prog/SRMService/inst/shiny/2Group2Test', port = 1234, host=130.60.81.134)"
+rem shiny::runApp("inst/shiny/2Group2Test",port=1234, host="130.60.81.134")
+R -e "shiny::runApp('C:/Users/wolski/prog/SRMService/inst/shiny/2Group2Test/', port = 1234, host='130.60.81.134')"
