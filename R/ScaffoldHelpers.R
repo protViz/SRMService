b9bae593f03dd99af01db88ddaefa2924dd3308a
Witold Wolski <wewolski@gmail.com>
2016-11-10 16:35:18 +0100

    document scaffold support

--- R/ScaffoldHelpers.R
+++ R/ScaffoldHelpers.R
@@ -1,8 +1,16 @@
+#' fix scaffold file
+#' @export
+#'
 fixScaffoldQuantReport <- function(filename){
   scftab <- read.csv(filename,sep="\t", stringsAsFactors = FALSE,skip=2)
   scftab <- scftab[-nrow(scftab),]
   tmp <- colnames(scftab)[2:ncol(scftab)]
   scftab <- scftab[,-ncol(scftab)]
   colnames(scftab) <- tmp
+
+  ACC<-gsub(" \\(\\+[0-9]+\\)$","",scftab$Accession.Number)
+  ACC<-gsub(" \\[[0-9]\\]$","",ACC)
+  rownames(scftab) <- ACC
+
   return(scftab)
 }
