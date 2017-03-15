7c4d1950ce0559f4c970ed9c3b1e45875bf0f142
Witold Wolski <wewolski@gmail.com>
2017-03-15 10:28:23 +0100

    startscript

---
+++ qcApplication.bat
@@ -0,0 +1,3 @@
+"R.exe" -e "Sys.setenv(RSTUDIO_PANDOC='C:/Program Files/RStudio/bin/pandoc');shiny::runApp('C:/Users/wolski/prog/SRMService/inst/shiny/QCApp', port = 1235, host='130.60.81.134')"
+
+rem will run QC report on server 130.60.81.134:1235
