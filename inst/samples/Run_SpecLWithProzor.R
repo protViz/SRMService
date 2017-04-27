476a3d7ddecbb4085e044dc44e9d783662121c0e
Witold Wolski <wewolski@gmail.com>
2017-04-27 08:43:38 +0200

    adding blibbuild to library generation script.

--- inst/samples/Run_SpecLWithProzor.R
+++ inst/samples/Run_SpecLWithProzor.R
@@ -2,19 +2,39 @@ library(prozor)
 library(specL)
 library(bibliospec)
 
-#BLIB_REDUNDANT <- "test0.9.blib"
-#BLIB_FILTERED <- "test0.9.filtered.blib"
+# you start with a folder containing your dat file.
 
 
-NON_REDUNDANT <- "test0.9.filtered.blib"
-REDUNDANT <- "test0.9.blib"
+# find out which fasta file was used. You will need this file to annotate the peptides
+DATFILE <- "F251145.dat"
+xx <-readLines(DATFILE, n=1000)
+grep("fastafile=",xx,value = TRUE)
+
+# path to fasta file
+FASTA_FILE <- "D:/projects/p2244_MilenaS_PN/library/fgcz_9606_d_reviewed_cnl_contaminantNoHumanCont_20161209.fasta"
 
 
+# How your library should be called and where stored:
+OUTPUTDIR = "output"
+NON_REDUNDANT <- "F251145.filtered.sqlite3"
+REDUNDANT <- "F251145.redundant.sqlite3"
+SWATH_LIBRARY <- "test_MeOHMerged.tsv"
 
-OUTPUTDIR = "C:/Users/wolski/prog/generateSpecLibrary/res.49562"
-FASTA_FILE <- "D:/projects/p2069/data/fasta/p2069_db1_d_20160322.fasta"
+# ion library parameters
 MAX_IONS <- 6
 MIN_IONS <- 5
 MZ_ERROR <- 0.05 # e.g for Q-Exactive
 MASCOT_MIN_SCORE <- 17
-IRT_PEPTIDES <- rename(bibliospec::CiRTpeptides, c("iRT"="rt"))
+IRT_PEPTIDES <- plyr::rename(bibliospec::CiRTpeptides, c("iRT"="rt"))
+
+
+# generate bibliospec files
+bb<-bibliospec::Blib()
+bb$build(idfiles = DATFILE, outfile = REDUNDANT,cutoff = 0.9)
+bb$filter(infile = REDUNDANT, outfile = NON_REDUNDANT)
+
+
+
+rmarkdown::render("specLWithProzor.Rmd")
+
+
