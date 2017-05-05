2418731399a43820eaf0abbfa699653d12974884
Witold Wolski <wewolski@gmail.com>
2017-05-05 14:23:48 +0200

    print

--- inst/samples/Run_QuantQCReport.R
+++ inst/samples/Run_QuantQCReport.R
@@ -1,8 +1,10 @@
 #
-# If you are going to use results produced by the scripts please do cite the
-# SRMSerivce R package by providing the following URL
+# If you are going to use results produced by the scripts please do cite:
+# SRMSerivce R package
 # www.github.com/protViz/SRMService
 # by W.E. Wolski, J. Grossmann, C. Panse
+# FGCZ
+
 #
 
 rm(list=ls())
@@ -11,23 +13,29 @@ library(qvalue)
 library(SRMService)
 
 
-protein <- read.table(("inst/samples/proteinGroups/proteinGroups.txt"),
+# max na per protein
+maxNA <- 3
+# min number of peptides per protein
+nrPeptides <-2
+# Experiment name (will appear as title in the pdf)
+experimentName <- "p2244_MilenaS_PN_HvsR"
+
+
+
+
+### Do not edit (except you know what you are doing)
+protein <- read.table("proteinGroups.txt",
                       sep="\t",
                       stringsAsFactors = F,
                       header=T)
 
-
-rawF <- gsub("Intensity\\.", "", grep("Intensity\\.",colnames(protein),value=T) )
-
-
 tmp <- cumsum(rev(table(protein$Peptides)))
 barplot(tmp[(length(tmp)-5):length(tmp)],ylim=c(0, length(protein$Peptides)),xlab='nr of proteins with at least # peptides')
 
-
-grp2 <- QCProteinReport( "p2244_MilenaS_PN_HvsR", maxNA=3  , nrPeptides=2)
-
+grp2 <- QCProteinReport(experimentName, maxNA=maxNA  , nrPeptides=nrPeptides)
 grp2$setMQProteinGroups(protein)
 
-rmarkdown::render("inst/reports/QCReport.Rmd","pdf_document")
+
+rmarkdown::render("QCReport.Rmd","pdf_document")
 
 
