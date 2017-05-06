5290fff4d1de85bfa5e845aea1d2fd8fda4307a3
Christian Panse <cp@fgcz.ethz.ch>
2017-05-06 11:40:54 +0200

    fix make vars global

--- R/fgcz.R
+++ R/fgcz.R
@@ -20,21 +20,20 @@
    RnwFile <- paste(reportFileBaseName, "Rnw", sep='.')
    texFile <- paste(reportFileBaseName, "tex", sep='.')
    
+   msms_d <<- read.table(msmsName, header = T, sep="\t")
+   summ <<- read.table(summary, header = F, sep="\t")
+   evi_d <<- read.table(evidence, header = T, sep="\t")
+   Fulldat <<- read.table(proteinGroups, header=T, sep="\t")
    
-   msms_d <- read.table(msmsName, header = T, sep="\t")
-   summ <- read.table(summary, header = F, sep="\t")
-   evi_d <- read.table(evidence, header = T, sep="\t")
-   Fulldat <- read.table(proteinGroups, header=T, sep="\t")
+   dat <<- Fulldat[,grep("^Intensity\\.", colnames(Fulldat))]
+   rownames(dat) <<- Fulldat$Majority.protein.IDs
    
-   dat <- Fulldat[,grep("^Intensity\\.", colnames(Fulldat))]
-   rownames(dat) <- Fulldat$Majority.protein.IDs
+   bool_moreThanOnePeptide <<- Fulldat$Razor...unique.peptides > 1
    
-   bool_moreThanOnePeptide <- Fulldat$Razor...unique.peptides > 1
-   
-   params <- read.table(parameters, header = TRUE, sep = "\t")
+   params <<- read.table(parameters, header = TRUE, sep = "\t")
    
    # peptides
-   pepts <- read.table(peptides, header = TRUE, sep = "\t")
+   pepts <<- read.table(peptides, header = TRUE, sep = "\t")
    
    # this is for witold to be replaced
    #fixedProteingroups <- "proteinGroups_FGCZ2grp_Intensity.txt"
