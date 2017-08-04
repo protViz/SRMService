49005512814ed8d067ac5a0c336e6ca4f660dc81
Witold Wolski <wewolski@gmail.com>
2017-08-04 11:12:28 +0200

    adding limma helpers

---
+++ R/limmaHelpers.R
@@ -0,0 +1,46 @@
+#' runs limma and computes qvalues
+#' @param intMatrix intensity matrix
+#' @param designMatrix design matrix
+#' @return list with results
+#' @export
+#' @importFrom limma lmFit
+#' @importFrom qvalue qvalue
+#'
+multigroupEBaysQvalue <- function(intMatrix, designMatrix){
+  fit2 <- lmFit(intMatrix, designMatrix)
+  fit2.eb <- eBayes(fit2)
+
+  coefsigma <- with(fit2.eb, sweep(coefficients,1, sigma, "/"))
+  t.ord <- with(fit2.eb, coefsigma/ stdev.unscaled )
+
+  p.ord <- 2 * stats::pt(-abs(t.ord), fit2.eb$df.residual)
+  p.mod <- fit2.eb$p.value
+
+  q.ord <- qvalue::qvalue(p.ord)$q
+  q.mod <- qvalue::qvalue(p.mod)$q
+
+  coef <- (fit2.eb$coefficients)
+
+  allElem <- list(intMatrix =intMatrix, effects = coef,
+                  q.mod = q.mod,
+                  q.ord = q.ord,
+                  p.mod = p.mod,
+                  p.ord = p.ord)
+  allElem <- lapply(allElem, quantable::matrix_to_tibble)
+  return(allElem)
+}
+#' merges results produced by multigroupEBaysQvalue
+#' @param multigrpBayes result of multigroupEBaysQvalue function
+#' @return data frame
+#' @export
+#' @importFrom plyr join_all
+mergeLimmEBayesResult <- function(multigrpBayes){
+  colnames(multigrpBayes$effects)[2:ncol(multigrpBayes$effects)] <- paste("Effect", colnames(multigrpBayes$effects)[2:ncol(multigrpBayes$effects)] , sep=".")
+  colnames(multigrpBayes$q.mod)[2:ncol(multigrpBayes$q.mod)]  <- paste("q.mod", colnames(multigrpBayes$q.mod)[2:ncol(multigrpBayes$q.mod)], sep=".")
+  colnames(multigrpBayes$q.ord)[2:ncol(multigrpBayes$q.ord)] <- paste("q.ord", colnames(multigrpBayes$q.ord)[2:ncol(multigrpBayes$q.ord)],  sep=".")
+  colnames(multigrpBayes$p.ord)[2:ncol(multigrpBayes$p.ord)] <- paste("p.ord", colnames(multigrpBayes$p.ord)[2:ncol(multigrpBayes$p.ord)],  sep=".")
+  colnames(multigrpBayes$p.mod)[2:ncol(multigrpBayes$p.mod)] <- paste("p.mod", colnames(multigrpBayes$p.mod)[2:ncol(multigrpBayes$p.mod)],  sep=".")
+  colnames(multigrpBayes$intMatrix)[2:ncol(multigrpBayes$intMatrix)] <- paste("Intensity", colnames(multigrpBayes$intMatrix)[2:ncol(multigrpBayes$intMatrix)], sep=".")
+  allData <- join_all(multigrpBayes, by="row.names")
+  return(allData)
+}
