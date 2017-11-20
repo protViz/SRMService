e0a167b0ea03397c6187952b6f72ae9051dab789
Witold Wolski <wewolski@gmail.com>
2017-11-20 17:38:47 +0100

    msstatsmappings

--- R/MSStatsMappings.R
+++ R/MSStatsMappings.R
@@ -1,10 +1,15 @@
-MSstatsMapping <- list("ProteinName" = "Protein.Name",
-  "PeptideSequence" = "Peptide.Sequence",
-  "PrecursorCharge" = "Precursor.Charge",
-  "FragmentIon" = "Fragment.Ion",
-  "ProductCharge" = "Product.Charge",
-  "IsotopeLabelType"="Isotope.Label",
-  "Intensity"="Area",
-  "Condition"="Condition",
-  "BioReplicate"="Sample.Name",
-  "Run" = "Replicate.Name")
+#' mapping of MSStats file names
+#' @export
+MSstatsMapping <- function(){
+  MSstatsMapping <- list("ProteinName" = "Protein.Name",
+                         "PeptideSequence" = "Peptide.Sequence",
+                         "PrecursorCharge" = "Precursor.Charge",
+                         "FragmentIon" = "Fragment.Ion",
+                         "ProductCharge" = "Product.Charge",
+                         "IsotopeLabelType"="Isotope.Label",
+                         "Intensity"="Area",
+                         "Condition"="Condition",
+                         "BioReplicate"="Sample.Name",
+                         "Run" = "Replicate.Name")
+  return(MSstatsMapping)
+}
