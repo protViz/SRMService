ee79619bdbebb4fedd44b0c19adec00783a3c8d8
Witold Wolski <wewolski@gmail.com>
2018-08-13 14:34:40 +0200

    test removed

--- tests/testthat/test-renderReports.R
+++ tests/testthat/test-renderReports.R
@@ -5,6 +5,9 @@ tmp_d <- tempdir()
 packagedir <- path.package("SRMService")
 
 test_that("source Run Tidy analysis", {
-  reportFile <- file.path(packagedir, "inst" , "RunScripts" , "Run_TidyAnalysis_Skyline_PRM.R")
-  source(reportFile)
+  #reportFile <- file.path(packagedir,
+  #                        "inst" ,
+  #                        "RunScripts" ,
+  #                        "Run_TidyAnalysis_Skyline_PRM.R")
+  # source(reportFile)
   })
