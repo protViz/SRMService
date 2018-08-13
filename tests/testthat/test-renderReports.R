be76b7d06455bd6b53d2f3e6044979ca50f236cc
Witold Wolski <wewolski@gmail.com>
2018-08-13 14:38:10 +0200

    test

--- tests/testthat/test-renderReports.R
+++ tests/testthat/test-renderReports.R
@@ -6,8 +6,9 @@ packagedir <- path.package("SRMService")
 
 test_that("source Run Tidy analysis", {
   #reportFile <- file.path(packagedir,
-  #                        "inst" ,
-  #                        "RunScripts" ,
+  #                        "inst",
+  #                        "RunScripts",
   #                        "Run_TidyAnalysis_Skyline_PRM.R")
   # source(reportFile)
   })
+
