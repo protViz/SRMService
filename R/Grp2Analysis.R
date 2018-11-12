ff98053f9095cdc757b0789b39982656e8bba96b
LucasKookUZH <lucasheinrich.kook@uzh.ch>
2018-11-12 15:48:12 +0100

    simpleheatmap3.R now implemented to return both column and row dendrograms.  bug fixed.

--- R/Grp2Analysis.R
+++ R/Grp2Analysis.R
@@ -285,11 +285,12 @@ Grp2Analysis <- setRefClass("Grp2Analysis",
                                 tmpdata1 <- .self$getNormalized()$data
 
                                 # Clustering using quantable::simpleheatmap3()
-                                clustering <- quantable::simpleheatmap3(t(tmpdata1), margins=c(1,10), breaks = seq(-2.5,2.5, length=26), labCol = rownames(tmpdata1), labRow = colnames(tmpdata1), plot = F, nrOfClusters = 5)
-                                colnames(clustering) = c("TopProteinName", "ClusterID")
+                                clustering <- quantable::simpleheatmap3(t(tmpdata1), margins=c(1,10), breaks = seq(-2.5,2.5, length=26), labCol = rownames(tmpdata1), labRow = colnames(tmpdata1), plot = F, nrOfClustersRow = 5, nrOfClustersCol = 2)
+                                col_clustering <- clustering$Col
+                                colnames(col_clustering) = c("TopProteinName", "ClusterID")
 
                                 # Merging ResultTable with ClusterID data.frame
-                                results <- dplyr::inner_join(tmpdata, clustering, by = tmpdata$TopProteinName)
+                                results <- dplyr::inner_join(tmpdata, col_clustering, by = "TopProteinName")
                                 return(results)
                               }
                             )
